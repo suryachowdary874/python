7d47639a237fc99f17e3d6edeb409dc1ff2a0780
suryachowdary874 <chowdarysurya462@gmail.com>
2026-05-01 15:36:52 +0530

    Refactor and add new functionality to pythoncrt.py

--- pythoncrt.py
+++ pythoncrt.py
@@ -1,44 +1,63 @@
-"""#Puzzle
-legs=int(input("Enter number of legs: "))
-heads=int(input("Enter number of heads: "))
+""" x = 10
+for i in range (20,10,-2):
+    x=x+2
+    print("Hi")
+for i in "Hi":
+    print(x, end=" ") """
 
-flag=False
-for cows in range(0,heads+1):
-    hens=heads-cows
-    cal_legs=cows*2 + hens*2
-    if cal_legs==legs:
-        flag=True
-        break
-if flag:
-    print("cows: ",cows)
-    print("hen: ",hens)
+
+
+"""for i in range (9):
+    print("*",end=" ")
+print()
+for j in range (i):
+        print("*               *")
+for i in range (9):
+    print("*",end=" ")"""
+
+
+"""a=int(input("Enter any Number"))
+if a%2==0:
+    for i in range(1,11):
+        print("{} x {} = {}".format(i,a,i*a))
 else:
-    print("no solution")"""
+    for i in range(1,a+1):
+        print("{} x {} = {}".format(i,a,i*a))"""
 
-"""from datetime import date
-a=date(2005,12,15)
-b=date(2026,4,28)
-print("No of days lived: ",(b-a).total_seconds()/3600)"""
+#NO OF WORDS COUNT
+"""a = input("Enter Any sentence: ")
+c = 0
+for i in range(len(a)):
+    if (a[i]==" " and a[i+1]!= " "):
+        c=c+1
+print("Words:",c+1)"""
 
-"""import calendar
-# Taking year and month input from user
-year = int(input("Enter year: "))
-month = int(input("Enter month (1-12): "))
-# Display the calendar
-print("\n", calendar.month(year, month))"""
-
-"""a=2
-for i in range(2,5):
-    a=a+2
-    for j in range(4):
-        a=a+3
-    print(a)"""
-
-
-for i in range(100,1000):
-    c=0
-    for j in range(2,i):
-        if i%j==0:
-            c=c+1
-    if c==0:
-        print(i,end=" ")
+"""import random
+p1 = input("Enter player1: ")
+p2 = input("Enter player2: ")
+s1=10
+s2=10
+d1=random.randint(1,10)
+d2=random.randint(1,10)
+print("______________PLAYER1 TURN____________")
+while(True):
+    g1=int(input("Enter your guess: "))
+    s1=s1-1
+    if g1==d1:
+        break
+print("______________PLAYER2 TURN____________")
+while(True):
+    g2=int(input("Enter your guess: "))
+    s2=s2-1
+    if g2==d2:
+        break
+print("_______________SUMMARY_________________")
+print("{}:{}".format(p1,s1))
+print("{}:{}".format(p2,s2))
+print("________________________________________")
+if s1 > s2:
+    print("Winner is ",p1)
+elif s2>s1:
+    print("Winner is ",p2)
+else:
+    print("DRAW")"""
